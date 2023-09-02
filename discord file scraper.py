91b28f30edd088de2d149d69e894c5065844da86
slasha <101141407+slashest@users.noreply.github.com>
2023-09-02 23:21:15 +0100

    Strip comments and add onmessage

--- discord file scraper.py
+++ discord file scraper.py
@@ -4,21 +4,25 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = discord.Client()
 
-# Replace 'file_extension' with the file type you are interested in, ".jpg" or ".exe"
-file_extension = 'file_extension'
+file_extension = '.jpg'
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print('Logged in as {0.user}'.format(client))
+    print(f'Logged in as {client.user.name} ({client.user.id})')
 
 @client.event
 async def on_message(message):
     if message.attachments:
         for attachment in message.attachments:
             if attachment.filename.endswith(file_extension):
-                await attachment.save(os.path.join(os.getcwd(), attachment.filename))
-
-# Replace 'discord_server_id' with the ID of the server you want to scrape
+                save_path = os.path.join(os.getcwd(), attachment.filename)
+                await attachment.save(save_path)
+                print(f'Saved {attachment.filename} from {message.author} in {message.channel}')
+                
 client.run(os.getenv('DISCORD_TOKEN'))
